5e738d1eff4f01f5c2f9f0c27576c89060372658
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2017-05-16 23:46:18 +0700

    Use SimpleTableViewCell in SimpleTableViewController

--- SmoothScrollingSample/ViewControllers/SimpleTableViewController.swift
+++ SmoothScrollingSample/ViewControllers/SimpleTableViewController.swift
@@ -10,20 +10,35 @@ import UIKit
 
 final class SimpleTableViewController: UIViewController {
 
-	@IBOutlet fileprivate weak var tableView: UITableView!
+	@IBOutlet fileprivate weak var tableView: UITableView?
+	
+	var tableData = [String]()
 	
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // TODO: Register cells later.
+		
+		registerTableCells()
+		populateTableData()
+		
+		tableView?.reloadData()
     }
+	
+	private func registerTableCells() {
+		tableView?.register(SimpleTableViewCell.nib(), forCellReuseIdentifier: SimpleTableViewCell.identifier)
+	}
+	
+	private func populateTableData() {
+		
+		let rawData: [Int] = Array(1...20)
+		tableData = rawData.map { "\($0)" }
+	}
 	
 }
 
 extension SimpleTableViewController: UITableViewDataSource {
 	
 	func numberOfSections(in tableView: UITableView) -> Int {
-
+	
 		self.log(function: #function)
 		return 1
 	}
@@ -31,15 +46,23 @@ extension SimpleTableViewController: UITableViewDataSource {
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		
 		self.log(function: #function)
-		return 0
+		return tableData.count
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		
 		self.log(function: #function)
 		
-		// TODO: Add proper dequeing code later.
-		return UITableViewCell()
+		let cell = tableView.dequeueReusableCell(withIdentifier: SimpleTableViewCell.identifier, for: indexPath)
+		
+		guard let simpleCell = cell as? SimpleTableViewCell else {
+			return cell
+		}
+		
+		let text = tableData[indexPath.row]
+		simpleCell.configure(text: text)
+		
+		return simpleCell
 	}
 	
 }
