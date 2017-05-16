124c228ac7f1ddd85e8f3bfbb339dd39fe94de11
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2017-05-16 22:33:42 +0700

    Initial commit for SimpleTableViewController

---
+++ SmoothScrollingSample/ViewControllers/SimpleTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SimpleTableViewController.swift
+//  SmoothScrollingSample
+//
+//  Created by Ricardo Pramana Suranta on 5/16/17.
+//  Copyright © 2017 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+final class SimpleTableViewController: UIViewController {
+
+	@IBOutlet fileprivate weak var tableView: UITableView!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // TODO: Register cells later.
+    }
+	
+}
+
+extension SimpleTableViewController: UITableViewDataSource {
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		print(#function)
+		return 1
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		
+		print(#function)
+		return 0
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		print(#function)
+		
+		// TODO: Add proper dequeing code later.
+		return UITableViewCell()
+	}
+	
+}
