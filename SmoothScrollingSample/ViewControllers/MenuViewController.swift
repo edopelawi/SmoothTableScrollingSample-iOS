49a2fa17ab8069c1c80608d7f9a56f4a64c2bc76
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2017-05-16 22:05:09 +0700

    Initial commit for MenuViewController

---
+++ SmoothScrollingSample/ViewControllers/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuViewController.swift
+//  SmoothScrollingSample
+//
+//  Created by Ricardo Pramana Suranta on 5/16/17.
+//  Copyright © 2017 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }    
+
+}
