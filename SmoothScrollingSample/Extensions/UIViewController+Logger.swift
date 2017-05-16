c8f7f061a7d42f520ba6b26803b405c59522c010
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2017-05-16 23:09:36 +0700

    Initial commit for UIViewController+Logger

---
+++ SmoothScrollingSample/Extensions/UIViewController+Logger.swift
@@ -0,0 +1,34 @@
+//
+//  UIViewController+Logger.swift
+//  SmoothScrollingSample
+//
+//  Created by Ricardo Pramana Suranta on 5/16/17.
+//  Copyright © 2017 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+final class Logger {
+	
+	static let shared = Logger()
+	
+	private let dateFormatter: DateFormatter
+	private init() {
+		dateFormatter = DateFormatter()
+		dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSSS"
+	}
+	
+	func log(_ string: String) {
+		let currentTime = Date()
+		let logTime = dateFormatter.string(from: currentTime)
+		
+		print("[\(logTime)] \(string)")
+	}
+}
+
+extension UIViewController {
+	
+	func log(function: Selector) {
+		Logger.shared.log("\(function)")
+	}
+}
