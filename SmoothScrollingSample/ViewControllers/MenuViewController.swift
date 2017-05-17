e2baaac39f32e93673c4b38ca90791cfcede60e0
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2017-05-17 21:53:29 +0700

    Configure navigation bar appearance in MenuViewController

--- SmoothScrollingSample/ViewControllers/MenuViewController.swift
+++ SmoothScrollingSample/ViewControllers/MenuViewController.swift
@@ -12,12 +12,28 @@ final class MenuViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
 		title = "Menu"
+		
+		configureNavigationBar()
 	}
 	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
 		self.navigationController?.isNavigationBarHidden = true
+	}
+	
+	fileprivate func configureNavigationBar() {
+		guard let navigationBar = self.navigationController?.navigationBar else {
+			return
+		}
+		
+		navigationBar.barTintColor = self.view.backgroundColor
+		navigationBar.tintColor = UIColor.white
+		
+		navigationBar.titleTextAttributes = [
+			NSForegroundColorAttributeName: UIColor.white
+		]
 	}
 	
 	@IBAction fileprivate func navigateToSimpleTableSample(_ sender: Any) {
@@ -27,7 +43,7 @@ final class MenuViewController: UIViewController {
 		self.navigationController?.pushViewController(viewController, animated: true)
 	}
 
-	@IBAction func navigateToSocialMediaTimeline(_ sender: Any) {
+	@IBAction fileprivate func navigateToSocialMediaTimeline(_ sender: Any) {
 		// TODO: Implement the navigation later.
 	}
 }
