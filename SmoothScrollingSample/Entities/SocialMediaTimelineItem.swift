262eb98ce9fe903c08bb187ec1ea4ae124ac1383
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2017-05-17 22:55:40 +0700

    Initial commit for SocialMediaTimelineItem

---
+++ SmoothScrollingSample/Entities/SocialMediaTimelineItem.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaTimelineItem.swift
+//  SmoothScrollingSample
+//
+//  Created by Ricardo Pramana Suranta on 5/17/17.
+//  Copyright © 2017 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+struct SociaMediaTimeLineItem {
+	
+	// TODO: Add user later.
+	
+	let text: String?
+	let imageURL: URL?
+}
