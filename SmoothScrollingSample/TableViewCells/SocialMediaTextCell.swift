e333826587fb95e3acb64706c6db95c978713b24
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2017-05-17 22:52:26 +0700

    Initial commit for SocialMediaTextCell

---
+++ SmoothScrollingSample/TableViewCells/SocialMediaTextCell.swift
@@ -0,0 +1,23 @@
+//
+//  SocialMediaTextCell.swift
+//  SmoothScrollingSample
+//
+//  Created by Ricardo Pramana Suranta on 5/17/17.
+//  Copyright © 2017 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+final class SocialMediaTextCell: UITableViewCell {
+
+	static let identifier = "SocialMediaTextCell"
+	
+	@IBOutlet fileprivate weak var avatarImageView: UIImageView!
+	@IBOutlet fileprivate weak var nameLabel: UILabel!
+	@IBOutlet fileprivate weak var contentLabel: UILabel!
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
