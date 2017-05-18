b66abe58f1ed7afa93dda69a792f80851aec5c60
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2017-05-18 07:19:50 +0700

    Replaec SocialMediaCellViewModel's getUserAvatar with closure-based method

--- SmoothScrollingSample/ViewModels/SocialMediaCellViewModel.swift
+++ SmoothScrollingSample/ViewModels/SocialMediaCellViewModel.swift
@@ -10,6 +10,8 @@ import UIKit
 
 final class SocialMediaCellViewModel {		
 	
+	var userAvatar: UIImage?
+	
 	var userName: String {
 		return item.user.name
 	}
@@ -21,15 +23,42 @@ final class SocialMediaCellViewModel {
 	var contentImage: UIImage?
 	
 	fileprivate let item: SocialMediaTimelineItem
+	fileprivate var ongoingDataTasks = [String: URLSessionDataTask]()
+	
+	// MARK: - Public methods
 	
 	init(item: SocialMediaTimelineItem) {
 		self.item = item
 	}
 	
-	func getUserAvatar() -> UIImage? {
-		return getImage(for: item.user.avatarURL)
+	/// Fetchs user avatar from server. Will update this instance's `userAvatar` and execute `completion` with retrieved `UIImage?`.
+	func fetchUserAvatar(completion: @escaping (UIImage?) -> Void) {
+		
+		if let validImage = userAvatar {
+			completion(validImage)
+			return
+		}
+		
+		// TODO: Replace this with asynchronous method for better performance later.
+		userAvatar = getImage(for: item.user.avatarURL)
+		completion(userAvatar)
 	}
 	
+	/// Fetchs content image from server. Will update this instance's `contentImage` and execute `completion` with retrieved `UIImage?`.
+	func fetchContentImage(completion: @escaping (UIImage?) -> Void) {
+	
+		if let validImage = contentImage {
+			completion(validImage)
+			return
+		}
+		
+		userAvatar = getImage(for: item.imageURL)
+		completion(userAvatar)
+	}
+	
+	// MARK: - Private methods
+	
+	/// Synchronous method to retrieve image from URL.
 	private func getImage(for url: URL?) -> UIImage? {
 		
 		guard let validURL = url,
@@ -38,5 +67,46 @@ final class SocialMediaCellViewModel {
 		}
 		
 		return UIImage(data: data)
+	}
+	
+	private func fetchImage(for url: URL?, completion: @escaping (UIImage?) -> Void) {
+		
+		guard let validURL = url else {
+			completion(nil)
+			return
+		}
+		
+		let dataTaskID = "\(Date())"
+		
+		let dataTask = URLSession.shared.dataTask(with: validURL) { [weak self] (data: Data?, _, _) in
+			
+			self?.removeDataTask(withID: dataTaskID)
+			
+			guard let validData = data else {
+				completion(nil)
+				return
+			}
+			
+			let image = UIImage(data: validData)
+			completion(image)
+		}
+		
+		
+		self.add(dataTask: dataTask, withID: dataTaskID)
+		dataTask.resume()
+	}
+	
+	private func add(dataTask: URLSessionDataTask, withID id: String) {
+		
+		DispatchQueue.main.async { [weak self] in
+			self?.ongoingDataTasks[id] = dataTask
+		}
+	}
+	
+	private func removeDataTask(withID id: String) {
+		
+		DispatchQueue.main.async { [weak self] in
+			let _ = self?.ongoingDataTasks.removeValue(forKey: id)
+		}
 	}
 }
