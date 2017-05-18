740d56711670b1c85199329431d181d35943da36
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2017-05-18 08:42:06 +0700

    Add SocialMediaCell protocol

---
+++ SmoothScrollingSample/TableViewCells/Protocols/SocialMediaCell.swift
@@ -0,0 +1,13 @@
+//
+//  SocialMediaCell.swift
+//  SmoothScrollingSample
+//
+//  Created by Ricardo Pramana Suranta on 5/18/17.
+//  Copyright © 2017 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+protocol SocialMediaCell {	
+	func configure(viewModel: SocialMediaCellViewModel)
+}
